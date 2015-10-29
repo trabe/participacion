78d3c1853e2627ec81e4da63d1649e737b91f730
Lucas Andión <lucas.andion@trabesoluciones.com>
2015-10-29 11:35:48 +0100

    Set galician as only language, delete es+en translations

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,5 +79,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.i18n.available_locales = [:es]
+  config.i18n.available_locales = [:gl]
 end

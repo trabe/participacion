072bba3afe17b1626c49568038a018709e1b17b0
Lucas Andión <lucas.andion@trabesoluciones.com>
2015-11-03 20:17:42 +0100

    Add some alias to branch while deploying just in case

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,7 +1,7 @@
 set :deploy_to, deploysecret(:deploy_to)
 set :server_name, deploysecret(:server_name)
 set :db_server, deploysecret(:server)
-set :branch, ENV['BRANCH'] || 'master'
+set :branch, ENV['BRANCH'] || ENV['branch'] || ENV['tag'] || 'master'
 set :ssh_options, port: deploysecret(:ssh_port)
 set :stage, :production
 set :rails_env, :production

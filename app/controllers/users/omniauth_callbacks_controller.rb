019b8515ed752d97ff5d95421f5638e838ad9e5a
Lucas Andión <lucas.andion@trabesoluciones.com>
2015-11-03 13:42:28 +0100

    Set 'usuario da UDC' instead of generic 'cas' on login flash. Drop omniauth-related login generalization, not used anymore

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,15 +1,16 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
-  def twitter
-    sign_in_with :twitter_login, :twitter
-  end
+  # TODO: Get name and email from cas.udc
+  def cas
+    @user = User.find_for_oauth(env["omniauth.auth"], current_user)
 
-  def facebook
-    sign_in_with :facebook_login, :facebook
-  end
-
-  [:cas].each do |provider|
-    provides_callback_for provider
+    if @user.persisted?
+      sign_in_and_redirect @user, event: :authentication
+      set_flash_message(:notice, :success, kind: t('omniauth.cas.user_kind') ) if is_navigational_format?
+    else
+      session["devise.cas_data"] = env["omniauth.auth"]
+      redirect_to new_user_registration_url
+    end
   end
 
   def after_sign_in_path_for(resource)

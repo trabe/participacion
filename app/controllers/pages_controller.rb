aa6ebe94f243f55c66009113cdc36bc3a6297ef6
Lucas Andión <lucas.andion@trabesoluciones.com>
2015-11-06 10:58:08 +0100

    Trimmed /more-info as much as possible to be a one-page layout with minimal info

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,40 +1,19 @@
 class PagesController < ApplicationController
   skip_authorization_check
 
-  def census_terms
-  end
-
   def conditions
-  end
-
-  def general_terms
   end
 
   def privacy
   end
 
-  def coming_soon
-  end
-
-  def how_it_works
-  end
-
-  def how_to_use
-  end
-
   def more_information
   end
 
-  def proposals_info
-  end
-
-  def participation_facts
-  end
-
-  def participation_world
-  end
-
   def verify
+  end
+
+  def coming_soon
   end
 
 end

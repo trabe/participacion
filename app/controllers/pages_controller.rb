d32a4436246c37a955dd2e4d08980ac468434957
Lucas Andión <lucas.andion@trabesoluciones.com>
2016-03-03 10:35:53 +0100

    Rolled back some abalde changes. Using udc social & email data. Removed Madrid pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,19 +1,10 @@
 class PagesController < ApplicationController
   skip_authorization_check
 
-  def conditions
-  end
-
   def privacy
   end
 
   def more_information
-  end
-
-  def verify
-  end
-
-  def coming_soon
   end
 
 end
